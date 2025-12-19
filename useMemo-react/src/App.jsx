fc7c3bd4577db33f01fdd1f24b9e17f50b40c2ce
Aanshi Dobariya <aanshidobariya.107@gmail.com>
2025-12-19 12:26:21 +0530

    useMemo-done

---
+++ useMemo-react/src/App.jsx
@@ -0,0 +1,38 @@
+import { useMemo, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(0);
+
+  function expensiveTask(num) {
+    console.log("Inside Expensive Task : ");
+    // for (let i = 0; i <= 1000000000000; i++) {}
+    return num * 2;
+  }
+
+  let doubleValue = expensiveTask(input);
+
+  // let doubleValue = useMemo(() => expensiveTask(input), [input]);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <br />
+      <br />
+      <div>Count: {count}</div>
+       <br />
+
+      <input
+        type="text"
+        placeholder="enter number : "
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <div>Double: {doubleValue}</div>
+    </div>
+  );
+}
+
+export default App;
